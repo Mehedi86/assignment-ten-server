5ba27a4d069fca65eac76c84d31098062a6e61f6
Mehedi86 <abdullahalmehedi@gmail.com>
2025-03-05 16:44:41 +0600

    added: review delete

--- index.js
+++ index.js
@@ -110,6 +110,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/updateReviews/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
+
         app.get('/reviews/yourReview/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email }
